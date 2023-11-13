9198ca48ff7a2fbb4d91e30c18563f1a7b9baede
Gandalf-Le-Dev <matproz.gaming@gmail.com>
2023-11-13 15:56:12 +0100

    Add task filtering by ID and refactor task listing.

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -4,6 +4,7 @@ import (
 	todogo "github.com/Gandalf-Le-Dev/TodoGo/core"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // listCmd represents the list command
@@ -13,12 +14,35 @@ var ListCmd = &cobra.Command{
 	Long:  `A longer description.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Info().Msg("list called")
-		tasks, err := todogo.ListTasks()
-		if err != nil {
-			log.Error().Msg(err.Error())
+
+		id := viper.GetInt("id")
+
+		if id != 0 {
+			log.Debug().Msgf("Getting task with id %d", id)
+			task, err := todogo.GetTask(id, viper.GetString("path"))
+			if err != nil {
+				log.Error().Msgf("Error while getting task with id %d: %s", id, err.Error())
+				return
+			} else if task == nil {
+				log.Error().Msgf("No task with id %d", id)
+				return
+			}
+			log.Trace().Msg(task.Format())
+		} else {
+			tasks, err := todogo.ListTasks(viper.GetString("path"))
+			if err != nil {
+				log.Error().Msgf("Error while listing tasks: %s", err.Error())
+				return
+			}
+			for _, task := range tasks {
+				log.Trace().Msg(task.Format())
+			}
 		}
-		for _, task := range tasks {
-			log.Info().Msg(task.Print())
-		}
+
 	},
+}
+
+func init() {
+	ListCmd.Flags().IntP("id", "i", 0, "List only tasks with id equal to value of flag. Usage: --id 1")
+	viper.BindPFlag("id", ListCmd.Flags().Lookup("id"))
 }
